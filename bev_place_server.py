d5c08d32a0e689d2fde3b0c0e785a92ed5497a5b
Manuj Naman <manujnaman@anantak.com>
2024-07-27 12:44:45 -0500

    Server has SIGINT handler, can serve without periodic waiting

--- bev_place_server.py
+++ bev_place_server.py
@@ -5,6 +5,7 @@ import os
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = "python" 
 
 import sys
+import signal
 import argparse
 import zmq
 import json 
@@ -30,6 +31,7 @@ from network.groupnet import group_config
 
 sys.path.append('/home/ubuntu/Anantak/Admin/messaging');
 import sensor_messages_pb2
+
 
 def input_transform():
     return transforms.Compose([
@@ -67,19 +69,22 @@ class SingleImageDataset(Dataset):
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 parser = argparse.ArgumentParser(description='BEVPlace')
-parser.add_argument('--test_batch_size', type=int, default=8, help='Batch size for testing')
+parser.add_argument('--listenPort', type=int, default=51705, help='port to use for listening for images')
+parser.add_argument('--publishPort', type=int, default=51706, help='port to use for publishing back')
+parser.add_argument("--periodic", type=bool, default=False, help="Run periodically at 2Hz with noblock")
 parser.add_argument('--nGPU', type=int, default=1, help='number of GPU to use.')
 parser.add_argument('--nocuda', action='store_true', help='Dont use cuda')
 parser.add_argument('--threads', type=int, default=4, help='Number of threads for each data loader to use')
 parser.add_argument('--resume', type=str, default='checkpoints/checkpoint_paper_kitti.pth.tar', help='Path to load checkpoint from, for resuming training or testing.')
-parser.add_argument('--listenPort', type=int, default=51705, help='port to use for listening')
-parser.add_argument('--publishPort', type=int, default=51706, help='port to use for listening')
 
-
+# Static BEV Place server
 class BEVPlaceServer:
 
     # Static variables of the class
     opt = parser.parse_args()
+
+    # How many images have been processed
+    num_images_processed = 0
 
     # ZMQ socket objects
     zmq_context = zmq.Context()
@@ -88,7 +93,6 @@ class BEVPlaceServer:
     zmq_sub_read_socket.setsockopt_string(zmq.SUBSCRIBE, "")
     zmq_sub_read_socket.setsockopt(zmq.CONFLATE, 1)
     zmq_sub_read_socket.connect(f"tcp://127.0.0.1:{ZMQ_sub_PORT}")
-
     ZMQ_pub_PORT = opt.publishPort
     zmq_pub_socket = zmq_context.socket(zmq.PUB)
     zmq_pub_url = f"tcp://127.0.0.1:{ZMQ_pub_PORT}"
@@ -115,7 +119,7 @@ class BEVPlaceServer:
     if cuda:
         model = nn.DataParallel(model)
         # model = model.to(device)
-
+    
     # Instance init
     def __init__(self):
         pass
@@ -154,19 +158,25 @@ class BEVPlaceServer:
         # img.show()
         # img.save('my_image.png')
 
+        encode_start_time = time.time()
         global_features = BEVPlaceServer.encodeImage(img)
+        encode_end_time = time.time()
         global_features_array = global_features.flatten()
-
         image_msg.description.extend(global_features_array)
-
         image_msg_bytes_str = image_msg.SerializeToString()
         BEVPlaceServer.zmq_pub_socket.send(image_msg_bytes_str)
         print(f"      Published image message back with descriptor")
+        publish_end_time = time.time()
+
+        BEVPlaceServer.num_images_processed += 1        
+        encoding_time_taken = encode_end_time - encode_start_time
+        publishing_time_taken = publish_end_time - encode_end_time
+        print(f"{BEVPlaceServer.num_images_processed} Encoding,publishing took {encoding_time_taken*1000:.0f}, {publishing_time_taken*1000:.0f} ms.")
 
         pass
 
     @staticmethod
-    def read_image():
+    def read_image_no_block():
         
         # Start subscription, read the messages, parse them and show them
 
@@ -188,6 +198,42 @@ class BEVPlaceServer:
             pass
 
 
+    @staticmethod
+    def read_image_forever():
+        
+        # Start subscription, read the messages, parse them and show them
+        while True:
+
+            error_msg_read = False
+            try:
+                image_msg_str = BEVPlaceServer.zmq_sub_read_socket.recv()
+                image_msg_read = True
+                # print(f'Read: {image_msg_str}')
+
+                # Parse the input message string
+                image_msg = sensor_messages_pb2.ImageMessage()
+                image_msg.ParseFromString(image_msg_str)
+                # print(f'Read: {reporter_msg}')
+
+                BEVPlaceServer.process_image_message(image_msg)
+
+            except Exception as e:
+                print(f"ERROR: {e}")
+                pass
+        
+        pass
+
+    @staticmethod
+    def ShutDown():
+
+        # Exit now
+        print("BEV Place model is unloading")
+        del BEVPlaceServer.model
+        print("Server is shutting down")
+
+        return
+
+
 # Global scheduler object
 msg_read_interval_sec = 0.5
 time_scheduler = sched.scheduler(time.time, time.sleep)
@@ -202,18 +248,28 @@ def repeatedly_run(_scheduler):
 
     if (last_msg_read_time + msg_read_interval_sec <= currtime):
         # print '  monitoring processes ', math.floor(currtime-starttime)
-        BEVPlaceServer.read_image()
+        BEVPlaceServer.read_image_no_block()
         last_monitor_processes_time = currtime
 
     # reenter
     time_scheduler.enter(msg_read_interval_sec, 1, repeatedly_run, (_scheduler,))
 
+
+# SigInt handler for exiting
+def signal_handler(sig, frame):
+    print('SIGINT received. Exiting...')
+    BEVPlaceServer.ShutDown()
+    sys.exit(0)
+
+# Main
 if __name__ == "__main__":
 
-    parser=argparse.ArgumentParser()
-    # parser.add_argument("--file", help="protoc filename to parse", type=str, default='')
-    # parser.add_argument("--live_map_num", help="number of the live map", type=int, default=-1)
     args=parser.parse_args()
 
-    time_scheduler.enter(0.5, 1, repeatedly_run, (time_scheduler,))
-    time_scheduler.run()
+    signal.signal(signal.SIGINT, signal_handler)
+
+    if args.periodic:
+        time_scheduler.enter(0.5, 1, repeatedly_run, (time_scheduler,))
+        time_scheduler.run()
+    else:
+        BEVPlaceServer.read_image_forever()
